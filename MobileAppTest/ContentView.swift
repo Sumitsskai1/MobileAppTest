2eb50830fd18710bf01b61dc1e9388a30d5390ef
kaifuchu1buil <masaki_sekiguchi@smtss.jp>
2023-03-03 10:56:24 +0900

    Initial Commit

---
+++ MobileAppTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MobileAppTest
+//
+//  Created by SuMiTS&S 開発第一部 on 2023/03/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MobileAppTest/MobileAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileAppTestApp.swift
+//  MobileAppTest
+//
+//  Created by SuMiTS&S 開発第一部 on 2023/03/03.
+//
+
+import SwiftUI
+
+@main
+struct MobileAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
